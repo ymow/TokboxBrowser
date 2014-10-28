72dbe3415852eca77b8ac571325952311ecce258
Ymow Wu <kenshin0812@hotmail.com>
2014-10-29 01:25:18 +0800

    root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
- root 'static_pages#Home'
-  get 'static_pages/home'
+ root:to => 'static_pages#home'
+ 
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
