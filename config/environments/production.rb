30a459e9c0912a990cc1c7c9f7e0ce69a9478a97
Ymow Wu <kenshin0812@hotmail.com>
2014-10-29 01:10:22 +0800

    config.assets.compile = true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,10 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  #config/environments/production.rb
+config.assets.compile = true
+# Heroku also requires this to be false
+config.assets.initialize_on_precompile=false
 
   # Generate digests for assets URLs.
   config.assets.digest = true
